7c196f11507b73eee442271b2661c6f245247494
Zafor Iqbal <xoy4444@gmail.com>
2024-08-03 16:34:15 +0600

    version 3

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const compression=require('compression')
 const morgan = require('morgan');
 
 
 module.exports=(app)=>{
+    app.use(compression())
     app.use(express.json());
     app.use(express.urlencoded({extended:true}))
 app.use(cors());
