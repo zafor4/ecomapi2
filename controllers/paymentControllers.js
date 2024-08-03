76a811370802dfbb7fe903c6b07265b5f23972f3
Zafor Iqbal <xoy4444@gmail.com>
2024-08-03 22:12:21 +0600

    i am done

--- controllers/paymentControllers.js
+++ controllers/paymentControllers.js
@@ -1,116 +1,100 @@
-const PaymentSession=require('ssl-commerz-node').PaymentSession
-const { CartItem}=require('../models/cartItem')
-const { Profile } = require('../models/profile')
-const {Order}=require('../models/Order')
-const {Payment}=require('../models/payment')
-const { v4: uuidv4 } = require('uuid');
+const { CartItem } = require('../models/cartItem');
+const { Profile } = require('../models/profile');
+const PaymentSession = require('ssl-commerz-node').PaymentSession;
+const { Order } = require('../models/Order');
+const { Payment } = require('../models/payment');
+
+// Request a Session
+// Payment Process
+// Receive IPN
+// Create an Order 
 
 module.exports.ipn = async (req, res) => {
-  try {
-      const payment = new Payment(req.body);
-      const tran_id = payment['tran_id'];
+    const payment = new Payment(req.body);
+    const tran_id = payment['tran_id'];
+    if (payment['status'] === 'VALID') {
+        const order = await Order.updateOne({ transaction_id: tran_id }, { status: 'Complete' });
+        await CartItem.deleteMany(order.cartItems);
+    } else {
+        await Order.deleteOne({ transaction_id: tran_id });
+    }
+    await payment.save();
+    return res.status(200).send("IPN");
 
-      if (payment['status'] === 'VALID') {
-          const order = await Order.findOne({ transaction_id: tran_id });
-
-          if (order) {
-              await Order.updateOne({ transaction_id: tran_id }, { status: 'Complete' });
-              await CartItem.deleteMany({ _id: { $in: order.cartItems } });
-          } else {
-              // Order does not exist
-              console.error(`Order with transaction_id ${tran_id} not found`);
-          }
-      } else {
-          await Order.deleteOne({ transaction_id: tran_id });
-      }
-
-      await payment.save();
-      res.status(200).send("IPN Received");
-  } catch (error) {
-      console.error('IPN Error:', error);
-      res.status(500).send("Internal Server Error");
-  }
 }
 
-
-
-
-
 module.exports.initPayment = async (req, res) => {
-    try {
-        const userId = req.user._id;
-        const cartItems = await CartItem.find({ user: userId });
-        const profile = await Profile.findOne({ user: userId });
+    const userId = req.user._id;
+    const cartItems = await CartItem.find({ user: userId });
+    const profile = await Profile.findOne({ user: userId });
 
-        if (!profile) {
-            return res.status(404).send("Profile not found");
-        }
+    const { address1, address2, city, state, postcode, country, phone } = profile;
 
-        const { address1, address2, city, state, postcode, country, phone } = profile;
+    const total_amount = cartItems.map(item => item.count * item.price)
+        .reduce((a, b) => a + b, 0);
 
-        const total_amount = cartItems.reduce((total, item) => total + (item.count * item.price), 0);
-        const total_item = cartItems.reduce((total, item) => total + item.count, 0);
-        const tran_id = uuidv4(); // Ensure tran_id is unique and not null
+    const total_item = cartItems.map(item => item.count)
+        .reduce((a, b) => a + b, 0);
 
-        console.log('Generated Transaction ID:', tran_id);
+    const tran_id = '_' + Math.random().toString(36).substr(2, 9) + (new Date()).getTime();
 
-        const payment = new PaymentSession(true, process.env.STORE_ID, process.env.STORE_PASSWORD);
-        payment.setUrls({
-            success: `${process.env.BASE_URL}/success`,
-            fail: `${process.env.BASE_URL}/fail`,
-            cancel: `${process.env.BASE_URL}/cancel`,
-            ipn: `${process.env.BASE_URL}/api/payment/ipn`,
-        });
+    const payment = new PaymentSession(true, process.env.STORE_ID, process.env.STORE_PASSWORD);
 
-        payment.setOrderInfo({
-            total_amount: total_amount,
-            currency: "BDT",
-            tran_id: tran_id,
-            emi_option: 0,
-        });
+    // Set the urls
+    payment.setUrls({
+        success: 'yoursite.com/success', // If payment Succeed
+        fail: 'yoursite.com/fail', // If payment failed
+        cancel: 'yoursite.com/cancel', // If user cancel payment
+        ipn: 'https://secret-stream-23319.herokuapp.com/api/payment/ipn' // SSLCommerz will send http post request in this link
+    });
 
-        payment.setCusInfo({
-            name: req.user.name,
-            email: req.user.email,
-            add1: address1,
-            add2: address2,
-            city: city,
-            state: state,
-            postcode: postcode,
-            country: country,
-            phone: phone,
-            fax: phone,
-        });
+    // Set order details
+    payment.setOrderInfo({
+        total_amount: total_amount, // Number field
+        currency: 'BDT', // Must be three character string
+        tran_id: tran_id, // Unique Transaction id 
+        emi_option: 0, // 1 or 0
+    });
 
-        payment.setShippingInfo({
-            method: "Courier",
-            num_item: total_item,
-            name: req.user.name,
-            add1: address1,
-            add2: address2,
-            city: city,
-            state: state,
-            postcode: postcode,
-            country: country,
-        });
+    // Set customer info
+    payment.setCusInfo({
+        name: req.user.name,
+        email: req.user.email,
+        add1: address1,
+        add2: address2,
+        city: city,
+        state: state,
+        postcode: postcode,
+        country: country,
+        phone: phone,
+        fax: phone
+    });
 
-        payment.setProductInfo({
-            product_name: "ecom Products",
-            product_category: "General",
-            product_profile: "general",
-        });
+    // Set shipping info
+    payment.setShippingInfo({
+        method: 'Courier', //Shipping method of the order. Example: YES or NO or Courier
+        num_item: total_item,
+        name: req.user.name,
+        add1: address1,
+        add2: address2,
+        city: city,
+        state: state,
+        postcode: postcode,
+        country: country,
+    });
 
-        const response = await payment.paymentInit();
-        const order = new Order({ cartItems: cartItems, user: userId, transaction_id: tran_id, address: profile });
+    // Set Product Profile
+    payment.setProductInfo({
+        product_name: 'Bohubrihi E-com Products',
+        product_category: 'General',
+        product_profile: 'general'
+    });
 
-        if (response.status === 'SUCCESS') {
-            order.sessionKey = response['sessionkey'];
-            await order.save();
-        }
-
-        res.status(200).send(response);
-    } catch (error) {
-        console.error('Payment Initialization Error:', error);
-        res.status(500).send("Internal Server Error");
+    response = await payment.paymentInit();
+    const order = new Order({ cartItems: cartItems, user: userId, transaction_id: tran_id, address: profile });
+    if (response.status === 'SUCCESS') {
+        order.sessionKey = response['sessionkey'];
+        await order.save();
     }
+    return res.status(200).send(response);
 }
